cddbb9bd25017a630a8e74a7c64ce66f74b23518
Ashish Gupta <ashishking554@gmail.com>
2026-07-01 23:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.47%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partition(self, head, x):
+        small_dummy = ListNode(0)
+        big_dummy = ListNode(0)
+
+        small = small_dummy
+        big = big_dummy
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+
+            head = head.next
+
+        # end big list
+        big.next = None
+
+        # connect small + big
+        small.next = big_dummy.next
+
+        return small_dummy.next
