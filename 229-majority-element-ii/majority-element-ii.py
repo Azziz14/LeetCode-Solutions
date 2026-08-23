dff91424e80ec9730debf779237044bba440611c
Ashish Gupta <ashishking554@gmail.com>
2026-08-24 02:21:30 +0530

    Time: 15 ms (9.72%) | Memory: 23.6 MB (27.28%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,17 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        n = len(nums)
-        threshold = n // 3
-        res = []
+        freq = {}
+        result = []
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+            if freq[i]>len(nums)//3 and i not in result:
+                result.append(i)
+        return result
         
-        count = 0
-        for i in range(n):
-            count += 1
-            # If we reached the end of a block of duplicates or the end of the list
-            if i == n - 1 or nums[i] != nums[i + 1]:
-                if count > threshold:
-                    res.append(nums[i])
-                count = 0  # Reset for next element group
-                
-        return res
