04927d915880b9e8987a250312ff24b4b25919b9
Ashish Gupta <ashishking554@gmail.com>
2026-08-24 23:26:48 +0530

    Time: 3 ms (60.64%) | Memory: 21 MB (93.63%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
         result = 0
-        
-        # XOR every number in the array
         for num in nums:
             result ^= num
             
