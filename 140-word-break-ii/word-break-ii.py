c7ced0705a24563e882bde4f1b5cb295261acc5f
Ashish Gupta <ashishking554@gmail.com>
2026-08-31 22:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (19.64%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -4,7 +4,6 @@ class Solution:
         memo = {}
         
         def dfs(start_idx):
-            # If we've reached the end of the string, return a list containing an empty string (representing a valid path)
             if start_idx == len(s):
                 return [""]
             
@@ -12,15 +11,11 @@ class Solution:
                 return memo[start_idx]
             
             valid_sentences = []
-            
-            # Try every possible prefix ending at end_idx
             for end_idx in range(start_idx + 1, len(s) + 1):
                 word = s[start_idx:end_idx]
                 if word in word_set:
-                    # Recursively find sentences for the remaining substring
                     sub_sentences = dfs(end_idx)
                     for sub in sub_sentences:
-                        # Append the current word with the rest of the sentence
                         if sub:
                             valid_sentences.append(word + " " + sub)
                         else:
