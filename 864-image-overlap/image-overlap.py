79d46d5ba078f27761da2960d47615eff6d146a1
Ashish Gupta <ashishking554@gmail.com>
2026-09-13 23:47:37 +0530

    Time: 182 ms (82.27%) | Memory: 19.8 MB (50.00%) - LeetSync

---
+++ 864-image-overlap/image-overlap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        l1 = [(r, c) for r in range(n) for c in range(n) if img1[r][c] == 1]
+        l2 = [(r, c) for r in range(n) for c in range(n) if img2[r][c] == 1]
+        
+        counts = collections.defaultdict(int)
+        for r1, c1 in l1:
+            for r2, c2 in l2:
+                counts[(r2 - r1, c2 - c1)] += 1
+                
+        return max(counts.values(), default=0)
